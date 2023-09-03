6a6bbe0d70c4b8bca61136e548af0166ac824383
BOUZEKOZANGUE <bouzekoadrienzangue@gmail.com>
2023-09-03 16:33:33 +0100

    TypeMaster

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
-// lancerJeu()
-let baliseZoneProposition = document.getElementById("zoneProposition");
-console.log(baliseZoneProposition);
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
 
-let listeInputRadio = document.querySelectorAll(".zoneChoix input");
-console.log(listeInputRadio);
+// Ici, j'ai mis la fonction lancerJeu() en commentaire pour ne pas être dérangé par l'affichage des popups. 
+lancerJeu()
 
-for (let i = 0; i < listeInputRadio.length; i++) {
-    console.log(listeInputRadio[i]);
-}
+// let inputEcriture = document.getElementById("inputEcriture")
+// console.log(inputEcriture)
+
+// let btnValiderMot = document.getElementById("btnValiderMot")
+// console.log(btnValiderMot)
+
+// let zoneProposition = document.querySelector(".zoneProposition")
+// console.log(zoneProposition)
+
+// let spanScore = document.querySelector(".zoneScore span")
+// console.log(spanScore)
+
+// let listeBtnRadio = document.querySelectorAll(".optionSource input")
+// console.log(listeBtnRadio)
--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,49 @@
- //affichage du score du l'utilisateur
-function afficherResultat(score,nbMotsProposer) {
-    console.log("Votre score est de " + score + " sur " + nbMotsProposer)
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les fonctions nécessaires au fonctionnement du jeu. 
+ * 
+ *********************************************************************************/
+
+/**
+ * Cette fonction affiche dans la console le score de l'utilisateur
+ * @param {number} score : le score de l'utilisateur
+ * @param {number} nbMotsProposes : le nombre de mots proposés à l'utilisateur
+ */
+function afficherResultat(score, nbMotsProposes) {
+    // Récupération de la zone dans laquelle on va écrire le score
+    let spanScore = document.querySelector(".zoneScore span")
+    // Ecriture du texte
+    let affichageScore = `${score} / ${nbMotsProposes}` 
+    // On place le texte à l'intérieur du span. 
+    spanScore.innerText = affichageScore
 }
 
-//fonction du choix
-function choisirPhraseOuMots() {
-    let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-        // Tant que l'utilisateur n'a pas saisi "mots" ou "phrases", on lui redemande de saisir un choix
-        while (choix !== "mots" && choix !== "phrases") {
-            choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-        }
-        return choix       
+function afficherProposition(proposition) {
+    let zoneProposition = document.querySelector(".zoneProposition")
+    zoneProposition.innerText = proposition
 }
-//fonction pour lancer la boucle du jeux
-function lancerBoucleDeJeux(listePropositions) {
-    let score = 0
-    for (let i = 0; i < listePropositions.length; i++) {
-        // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
-        let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
-        if (motUtilisateur === listePropositions[i]) {
-            score++
-        }
-    }
-    return score
-}
-//fonction pour lancer le jeux
+
+/**
+ * Cette fonction lance le jeu. 
+ * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
+ */
 function lancerJeu() {
-    let choix =choisirPhraseOuMots()
+    // Initialisations
     let score = 0
-    let nbMotsProposer = 0
+    let nbMotsProposes = 0
+    let i = 0
 
-    if (choix === 'mots') {
-        lancerBoucleDeJeux(listeMots)
-        nbMotsProposer = listeMots.length
-    } else {
-        lancerBoucleDeJeux(listePhrases)
-        nbMotsProposer = listePhrases.length
-    }
-    afficherResultat(score,nbMotsProposer)
+    //
+    let btnValiderMot = document.getElementById("btnValiderMot")
+    //
+    let inputEcriture =document.getElementById("inputEcriture")
+    afficherProposition(listeMots[i])
+    btnValiderMot.addEventListener("click",() =>{
+        console.log(inputEcriture.value)
+        i++
+        afficherProposition(listeMots[i])
+    })
+
+
+    afficherResultat(score, nbMotsProposes)
 }
- 
